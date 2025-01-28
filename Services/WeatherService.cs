a3be7c488801119e08cc69f9fae76bafb5145d44
Ndzalo <ndalomaluks@gmail.com>
2025-01-28 12:48:42 +0200

    Add project files.

---
+++ Services/WeatherService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WeatherAppp.Models;
+
+namespace WeatherAppp.Services
+{
+    public class WeatherService
+    {
+        private readonly HttpClient _httpClient;
+        private const string API_KEY = "e1f9d6724d88fae11fdca0efb8c54c26";
+
+        public WeatherService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<WeatherModel.WeatherResponse> GetWeatherByCoordinates(double latitude, double longitude)
+        {
+           // latitude = -23.883669;
+            //longitude = 29.707878;
+            var response = await _httpClient.GetAsync(
+                $"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={API_KEY}&units=metric");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return JsonSerializer.Deserialize<WeatherModel.WeatherResponse>(
+                    await response.Content.ReadAsStringAsync(),
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            }
+
+            throw new Exception("Failed to get weather data");
+        }
+
+        public async Task<WeatherModel.WeatherResponse> GetWeatherByCity(string city)
+        {
+            var response = await _httpClient.GetAsync(
+                $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return JsonSerializer.Deserialize<WeatherModel.WeatherResponse>(
+                    await response.Content.ReadAsStringAsync(),
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            }
+
+            throw new Exception("Failed to get weather data");
+        }
+    }
+}
